9d7f8f09ad50a31e6c4b9baa8b055250ceda42b9
im5hizu <thybault.maurice@gmail.com>
2023-04-06 14:55:10 +0200

    affichage article détaillé

--- js/productLoader.js
+++ js/productLoader.js
@@ -1,8 +1,10 @@
 const urlJson = "http://127.0.0.1:5500/json/products.json";
 const productSection = document.getElementById("produits");
-const singleProductSection = document .getElementById('produit-simple')
+const singleProductSection = document.getElementById("produit-simple");
 const ulProduct = document.createElement("ul");
-if(productSection){productSection.appendChild(ulProduct);}
+if (productSection) {
+  productSection.appendChild(ulProduct);
+}
 
 const loadProducts = async (urlJson) => {
   fetch(urlJson)
@@ -10,11 +12,9 @@ const loadProducts = async (urlJson) => {
     .then((responseJson) => {
       responseJson.products.forEach((product) => {
         // Appel de la fonction pour afficher les produits.
-        if(productSection){
+        if (productSection) {
           createProducts(product);
-        } else if (singleProductSection){
-          singleProductLoader(product);
-        }
+        };
       });
     })
     .catch((error) => console.error(error));
@@ -45,25 +45,4 @@ const createProducts = (product) => {
   productH3.innerHTML = `${product.name}`;
   productSpan.innerHTML = `${product.price}€`;
   hrefProduct2.href = `http://127.0.0.1:5500/produit-simple.html?id=${product.id}`;
-};
-
-const singleProductLoader = (product) => {
-  const contentDiv = document.getElementById("produit-simple");
-  contentDiv.innerHTML = `
-  <article>
-  <h3>${product.name}</h3>
-  <div>
-      <figure><img src="/img/products/${product.category}/${product.subCategory}/${product.image}"></figure>
-      <div>
-          <p></p>
-          ${product.description}
-          <p></p>
-          <footer>
-              <form id="panier"><input type="hidden" id="id" value="1"><input type="number" min="1" step="1" value="1"
-                      id="price"><span id="calcul-price">${product.price}€</span><button type="submit" class="bt-panier"
-                      id="bt-panier" href="panier.html?id=1">Ajouter au panier</button></form>
-          </footer>
-      </div>
-  </div>
-</article>`;
 };
---
+++ js/singleProduct.js
@@ -0,0 +1,41 @@
+const urlJson = "http://127.0.0.1:5500/json/products.json";
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const productId = urlParams.get('id');
+
+const loadProducts = async (urlJson) => {
+    fetch(urlJson)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        const products = responseJson.products
+        for(let i = 0; i < productId; i++){
+          let product = products[i]
+          singleProductLoader(product)
+        }
+    })
+  };
+
+const singleProductLoader = (product) => {
+  
+    console.log(productId);
+    const contentDiv = document.getElementById("produit-simple");
+    contentDiv.innerHTML = `
+    <article>
+    <h3>${product.name}</h3>
+    <div>
+        <figure><img src="/img/products/${product.category}/${product.subCategory}/${product.image}"></figure>
+        <div>
+            <p></p>
+            ${product.description}
+            <p></p>
+            <footer>
+                <form id="panier"><input type="hidden" id="id" value="1"><input type="number" min="1" step="1" value="1"
+                        id="price"><span id="calcul-price">${product.price}€</span><button type="submit" class="bt-panier"
+                        id="bt-panier" href="panier.html?id=1">Ajouter au panier</button></form>
+            </footer>
+        </div>
+    </div>
+  </article>`;
+  };
+
+  loadProducts(urlJson)
